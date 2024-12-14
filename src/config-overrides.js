8342e0e60b4d74b22b088d981b33344f8522eafd
unknown <welinsonmarlon15@gmail.com>
2024-12-14 20:50:34 -0300

    1.0

---
+++ src/config-overrides.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: function(config, env) {
+    config.resolve.fallback = {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+    };
+
+    return config;
+  },
+};
